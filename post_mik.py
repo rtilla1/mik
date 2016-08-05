2a774f6e683ae56d475c7247896c38e1a9cbe1f6
garett armstrong <garrett.armstrong@gmail.com>
2016-08-05 16:03:00 -0500

    merging all post-mik scripts into post_mik.py

--- post_mik.py
+++ post_mik.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from shutil import copyfile
 from lxml import etree as ET
 
 
@@ -83,6 +84,115 @@ class IsCountsCorrect():
                     compounds_count += len([i for i in files if i == "MODS.xml"])
                 return compounds_count
 
+
+class PullInBinaries():
+    '''
+    usage is:
+        from base directory of mik,
+        after mods successfully created,
+        (for either simple or compound or both),
+        shellcommand:
+                      'python3 pull_in_binaries.py p16313coll13'
+
+       result is:
+        for each mods item in the output/alias_simpleorcompound/original_structure/ directory,
+        the matching binary will be pulled from mik/Cached_Cdm_files/alias/
+
+       a tiny error message will print on the shell screen if no matching file is found
+    '''
+
+    def __init__(self, alias):
+        sourcefiles_paths = PullInBinaries.makedict_sourcefiles(alias)
+        simplexmls_list = PullInBinaries.makelist_simpleoutfolderxmls(alias)
+        compoundxmls_list = PullInBinaries.makelist_compoundoutfolderxmls(alias)
+        for filelist in (simplexmls_list, compoundxmls_list):
+            for kind, outroot, pointer in filelist:
+                if pointer not in sourcefiles_paths:
+                    if ("_compound" in outroot) and (outroot.split('/')[-2] == "original_structure"):
+                        continue  # root of cpd is expected to have no binary
+                    else:
+                        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                        print("{}.xml may not have a matching binary".format(pointer))
+                        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                if PullInBinaries.is_binary_in_output_dir(kind, outroot, pointer):
+                    continue
+                if pointer not in sourcefiles_paths:
+                    continue
+                sourcepath, sourcefile = sourcefiles_paths[pointer]
+                PullInBinaries.copy_binary(kind, sourcepath, sourcefile, outroot, pointer)
+
+    @staticmethod
+    def makedict_sourcefiles(alias):
+        sourcefiles_paths = dict()
+        input_dir = 'Cached_Cdm_files/{}'.format(alias)
+        for root, dirs, files in os.walk(input_dir):
+            for file in files:
+                if file.split('.')[-1] in ('jp2', 'mp4', 'mp3', 'pdf'):
+                    alias = file.split('.')[0]
+                    sourcefiles_paths[alias] = (root, file)
+        return sourcefiles_paths
+
+    @staticmethod
+    def makelist_simpleoutfolderxmls(alias):
+        xml_filelist = []
+        subfolder = "output/{}_simple/original_structure".format(alias)
+        for root, dirs, files in os.walk(subfolder):
+            for file in files:
+                if '.xml' in file:
+                    pointer = file.split('.')[0]
+                    xml_filelist.append(('simple', root, pointer))
+        return xml_filelist
+
+    @staticmethod
+    def makelist_compoundoutfolderxmls(alias):
+        xml_filelist = []
+        subfolder = "output/{}_compound/original_structure".format(alias)
+        for root, dirs, files in os.walk(subfolder):
+            for file in files:
+                if file == 'MODS.xml':
+                    pointer = root.split('/')[-1]
+                    xml_filelist.append(('compound', root, pointer))
+        return xml_filelist
+
+    @staticmethod
+    def is_binary_in_output_dir(kind, root, pointer):
+        acceptable_binary_types = ('mp3', 'mp4', 'jp2', 'pdf')
+        if kind == 'simple':
+            for filetype in acceptable_binary_types:
+                if os.path.isfile('{}/{}.{}'.format(root, pointer, filetype)):
+                    return True
+        if kind == 'compound':
+            for filetype in acceptable_binary_types:
+                if os.path.isfile('{}/{}/OBJ.{}'.format(root, pointer, filetype)):
+                    return True
+        return False
+
+    @staticmethod
+    def copy_binary(kind, sourcepath, sourcefile, outroot, pointer):
+        if kind == 'simple':
+            copyfile("{}/{}".format(sourcepath, sourcefile), "{}/{}".format(outroot, sourcefile))
+        elif kind == 'compound':
+            copyfile("{}/{}".format(sourcepath, sourcefile), "{}/OBJ.{}".format(outroot, sourcefile.split('.')[-1]))
+
+
+class MakeStructureFile():
+    def __init__(self, alias):
+        for root, dirs, files in os.walk('output'):
+            if 'structure.cpd' in files:
+                parent = root.split("/")[-1]
+                xml_header = '<?xml version="1.0" encoding="utf-8"?>'
+                new_etree = ET.Element("islandora_compound_object", title=parent)
+
+                old_etree = ET.parse("{}/structure.cpd".format(root))
+                for i in old_etree.findall('.//pageptr'):
+                    # print('child is: ', i.text)
+                    new_etree.append(ET.Element('child', content=i.text))
+
+                with open('{}/structure.xml'.format(root), 'wb') as f:
+                    f.write(ET.tostring(new_etree, encoding="utf-8", xml_declaration=True, pretty_print=True))
+
 if __name__ == '__main__':
     alias = sys.argv[1]
+    PullInBinaries(alias)
+    MakeStructureFile(alias)
     IsCountsCorrect(alias)
